700b69020a84c6ed85397680447592becf6fdddc
Nguyễn Văn Sang (Yue) <nvsang2670@gmail.com>
2023-12-15 18:03:07 +0700

    fix menu

--- src/component/header.js
+++ src/component/header.js
@@ -6,39 +6,41 @@ export default function Header() {
 
   return (
     <nav className="sticky top-0 z-10 bg-sky-800 border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="max-w-screen-2xl flex flex-wrap items-center mx-auto">
-        <a href="/" className="flex items-center pr-4">
-          {/* <img src={logo} className="h-8 mr-3" alt="Flowbite Logo" /> */}
-          <span className="self-center text-2xl font-bold whitespace-nowrap text-white">
-            MQL<span className="text-yellow-400">5</span>
-          </span>
-        </a>
-        <div className={"!flex justify-between"}>
-          <button
-            data-collapse-toggle="navbar-dropdown"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-dropdown"
-            aria-expanded="false"
-            onClick={() => setOpenNav(!openNav)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+      <div className="w-full flex flex-wrap items-center mx-auto">
+        <div className={'flex justify-between max-md:w-full'}>
+          <a href="/" className="flex items-center pr-4">
+            {/* <img src={logo} className="h-8 mr-3" alt="Flowbite Logo" /> */}
+            <span className="self-center text-2xl font-bold whitespace-nowrap text-white">
+              MQL<span className="text-yellow-400">5</span>
+            </span>
+          </a>
+          <div className={"!flex justify-between"}>
+            <button
+              data-collapse-toggle="navbar-dropdown"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-dropdown"
+              aria-expanded="false"
+              onClick={() => setOpenNav(!openNav)}
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
 
         <div className={`${openNav ? "" : "hidden"} w-full md:block md:w-auto`}>
