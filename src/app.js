2bb23af5eef6b625a1c2ef7680da8f3e8ee3a5f0
nguyensang <nguyensang@vnvn.net>
2023-12-20 15:53:48 +0700

    fix code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import { useLocation } from 'react-router-dom';
 import { DefaultLayoutAdmin, DefaultLayoutUser } from "./component";
+import { useCookies } from 'react-cookie';
 
 const roterAdmin = '/admin'
 function App() {
   const location = useLocation();
   const currentPath = location.pathname;
+  const [cookies] = useCookies(['user']);
+  console.log('cookies', cookies)
 
   if(currentPath?.includes(roterAdmin)) return <DefaultLayoutAdmin />
   return <DefaultLayoutUser />
